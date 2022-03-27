6e42f2689ee3a0903731b031f6d20bea7c27047e
Eugene Kirvas <kirea.my@gmail.com>
2022-03-27 14:47:39 +0300

    add mongodb

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,11 +15,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  Product.findAll({ where: { id: prodId } })
+    .then(products => {
       res.render('shop/product-detail', {
-        product,
-        pageTitle: product.title,
+        product: products[0],
+        pageTitle: products[0].title,
         path: '/products',
       });
     })
